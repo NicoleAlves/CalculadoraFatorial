e31100ce40b9242b832f19eb20555204b6b55f7c
NicoleFreitas <nicole.alves30@gmail.com>
2015-03-06 11:01:11 -0300

    Fatorial_Trabalho 1º Bimestre

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Fatorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MI = new System.Windows.Forms.Button();
+            this.MR = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // MI
+            // 
+            this.MI.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MI.Location = new System.Drawing.Point(15, 72);
+            this.MI.Name = "MI";
+            this.MI.Size = new System.Drawing.Size(95, 23);
+            this.MI.TabIndex = 0;
+            this.MI.Text = "Iterativa";
+            this.MI.UseVisualStyleBackColor = true;
+            this.MI.Click += new System.EventHandler(this.ClickI);
+            // 
+            // MR
+            // 
+            this.MR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MR.Location = new System.Drawing.Point(167, 72);
+            this.MR.Name = "MR";
+            this.MR.Size = new System.Drawing.Size(95, 23);
+            this.MR.TabIndex = 1;
+            this.MR.Text = "Recursiva";
+            this.MR.UseVisualStyleBackColor = true;
+            this.MR.Click += new System.EventHandler(this.ClickR);
+            // 
+            // label
+            // 
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(49, 109);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(175, 30);
+            this.label.TabIndex = 2;
+            // 
+            // txt
+            // 
+            this.txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt.Location = new System.Drawing.Point(15, 26);
+            this.txt.Multiline = true;
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(247, 26);
+            this.txt.TabIndex = 3;
+            this.txt.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // Fatorial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 148);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.MR);
+            this.Controls.Add(this.MI);
+            this.Name = "Fatorial";
+            this.Text = "Fatorial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MI;
+        private System.Windows.Forms.Button MR;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox txt;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Fatorial : Form
+    {
+        public double result;
+
+        public Fatorial()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickR(object sender, EventArgs e)
+        {
+            Button recur = sender as Button;
+            double Ab = Double.Parse(txt.Text);
+            label.Text = Convert.ToString(Recursiva(Ab));
+        }
+
+        private void ClickI(object sender, EventArgs e)
+        {
+            Button iter = sender as Button;
+            double Bc = Double.Parse(txt.Text);
+            label.Text = Convert.ToString(Iterativa(Bc));
+        }
+
+        public double Iterativa(double num)
+        {
+            double x = num;
+            for (double i = num-1; i > 0; i--)
+                {
+                    x *=  i;
+                }
+            return x;
+        }
+
+        public double Recursiva(double num)
+        {
+            if (num == 0) return 1;
+            result = num * Recursiva(num - 1);
+            return result;
+        }
+
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
